b07c23bff1a40f84e5560b5f67b65814e344f3c9
YH4-ES <yifanh4@nycstudents.net>
2026-01-27 14:02:17 -0500

    asdf

---
+++ start.js
@@ -0,0 +1,7 @@
+let rock = document.getElementById("rock")
+let paper = document.getElementById("paper")
+let scissors = document.getElementById("scissors")
+
+function randombotChoice(min, max) {
+    Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
