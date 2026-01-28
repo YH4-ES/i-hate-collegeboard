af18fe802a35339235b63270e17ded245f48a033
YH4-ES <yifanh4@nycstudents.net>
2026-01-28 14:01:50 -0500

    asdf

---
+++ base.js
@@ -0,0 +1,20 @@
+export const stagesetchoices = {
+    cerbChoices : [
+        botrock = {
+        counter : rpcOptions.paper,
+        beats : rpcOptions.scissors,
+        ties : rpcOptions.rock,
+        },
+        botpaper = {
+        counter : rpcOptions.scissors,
+        beats : rpcOptions.rock,
+        ties : rpcOptions.paper,
+        },
+        botscissors = {
+        counter: rpcOptions.rock,
+        beats: rpcOptions.paper,
+        ties: rpcOptions.scissors,
+        },
+    ],
+
+}
--- start.js
+++ start.js
@@ -1,7 +1,36 @@
-let rock = document.getElementById("rock")
-let paper = document.getElementById("paper")
-let scissors = document.getElementById("scissors")
+let rpcOptions = document.getElementsByClassName("option")
+
+let botchoices = [
+    botrock = {
+        counter : rpcOptions.paper,
+        beats : rpcOptions.scissors,
+        ties : rpcOptions.rock,
+    },
+    botpaper = {
+        counter : rpcOptions.scissors,
+        beats : rpcOptions.rock,
+        ties : rpcOptions.paper,
+    },
+    botscissors = {
+        counter: rpcOptions.rock,
+        beats: rpcOptions.paper,
+        ties: rpcOptions.scissors,
+    }
+]
 
 function randombotChoice(min, max) {
-    Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+let botchoice = randombotChoice(0, 2)
+
+function matchoptions(option){
+    if (option == botchoices[botchoice].counter){
+        console.log("WIN")
+    }
+}
+
+java.util.Properties = new Properties();
+p.setProperty("johndoe.pin", "12345");
+p.store(new FileWriter("myfile.properties"), "")
